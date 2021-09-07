c3f9fb316686b4c9ad7f5a95c0ee5f02d01ac91c
arthurehrat <85951693+arthurehrat@users.noreply.github.com>
2021-09-07 20:26:06 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,39 +12,36 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int codigo, quantidade;
 		double totalG = 0;
-		double total= 0;
-		int add = 1;
-		
+		double total = 0;
+		char add = 's';
+
 		System.out.println("Bem vindo a nossa lanchonete!\n ");
-		
-		while(add != 2) {
-		System.out.println("CODIGO     ITEM        PREÇO\n01   Cachorro Quente   R$ 4,00\n02     X-SALADA        R$ 4,50\n03     X-BACON         R$ 5,00\n04     TORRADA         R$ 2,00\n05     REFRIGERANTE    R$ 1,50\n"
-				);
-        System.out.println("Digite o numero do item desejado:\n");
-		codigo = sc.nextInt();
-		System.out.println("Digite a quantidade:");
-		quantidade = sc.nextInt();
-		
-		if (codigo == 1) {
-			total = quantidade*4.00;
+
+		while (add != 'n') {
+			System.out.println(
+					"CODIGO     ITEM        PREÇO\n01   Cachorro Quente   R$ 4,00\n02     X-SALADA        R$ 4,50\n03     X-BACON         R$ 5,00\n04     TORRADA         R$ 2,00\n05     REFRIGERANTE    R$ 1,50\n");
+			System.out.println("Digite o numero do item desejado:\n");
+			codigo = sc.nextInt();
+			System.out.println("Digite a quantidade:");
+			quantidade = sc.nextInt();
+
+			if (codigo == 1) {
+				total = quantidade * 4.00;
+			} else if (codigo == 2) {
+				total = quantidade * 4.50;
+			} else if (codigo == 3) {
+				total = quantidade * 5.00;
+			} else if (codigo == 4) {
+				total = quantidade * 2.00;
+			} else if (codigo == 5) {
+				total = quantidade * 1.50;
+			}
+			totalG = total + totalG;
+			System.out.printf("Total R$ %.2f%n", total);
+			System.out.println("Deseja adicionar mais itens?(S/N)");
+			add = sc.next().charAt(0);
+
 		}
-		else if (codigo == 2){
-			total = quantidade * 4.50;
-		}
-		else if (codigo == 3) {
-			total = quantidade *5.00;
-		}
-		else if (codigo == 4) {
-			total = quantidade * 2.00;
-		}
-		else if (codigo == 5) {
-			total = quantidade * 1.50;
-		}
-		totalG = total + totalG;
-		System.out.printf("Total R$ %.2f%n",total);
-		System.out.println("Deseja adicionar mais itens?Sim - 1 Não - 2");
-		add = sc.nextInt();		
-		} 
 		System.out.printf("Total= R$" + totalG);
 		sc.close();
 	}
